660ef01f56dd67cbac1331f45764cb34d06472e4
PJL <pauljohannlorenz@proton.me>
2024-12-02 12:16:33 +0100

    first commit

---
+++ src/main/java/com/example/rest_fse/RestFseApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_fse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFseApplication.class, args);
+	}
+
+}
