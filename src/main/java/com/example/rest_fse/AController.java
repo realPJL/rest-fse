60b57dc687e775c5b97351928ca39da90cf2cba3
PJL <pauljohannlorenz@proton.me>
2024-12-02 16:47:38 +0100

    Initial commit

---
+++ src/main/java/com/example/rest_fse/AController.java
@@ -0,0 +1,11 @@
+package com.example.rest_fse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+
+public class AController {
+    
+}
