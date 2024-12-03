b27b5cb9b936a866ec153fc602be45b975f0251a
PJL <pauljohannlorenz@proton.me>
2024-12-03 11:15:15 +0100

    Kommentar angepasst

--- src/main/java/com/example/rest_fse/ArtikelRepository.java
+++ src/main/java/com/example/rest_fse/ArtikelRepository.java
@@ -3,7 +3,7 @@ package com.example.rest_fse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//Für die Datenbankanbindung & // Standard-Methoden wie save, findAll, findById werden automatisch bereitgestellt.
+//Für die Datenbankanbindung & Standard-Methoden wie save, findAll, findById werden automatisch bereitgestellt
 @Repository
 public interface ArtikelRepository extends JpaRepository<Artikel, Long> {
 }
