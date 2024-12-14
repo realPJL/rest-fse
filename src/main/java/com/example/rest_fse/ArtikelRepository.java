94615dae4a94e7026500154677f02bdee448727f
enricowlk <enrico.woelck@gmail.com>
2024-12-15 00:42:04 +0100

    kommentar erweitert

--- src/main/java/com/example/rest_fse/ArtikelRepository.java
+++ src/main/java/com/example/rest_fse/ArtikelRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 //Für die Datenbankanbindung & Standard-Methoden wie save, findAll, findById werden automatisch bereitgestellt
 @Repository
 public interface ArtikelRepository extends JpaRepository<Artikel, Long> {
-    // Methode soll die Artikelliste abrufen
+    // Methode soll eine Artikelliste abrufen, wo alle Artikel enthalten sind die den gleichen Lagerort haben
     List<Artikel> findByLagerort(String lagerort);
     // Methode soll überprüfen, ob ein bestimmter Lagerort existiert
     boolean existsByLagerort(String lagerort);
